b9ac6815fbef511664f66e461e6085969ee7dcd4
shaharia0 <shaharia35-1964@diu.edu.bd>
2023-05-13 02:08:26 +0600

    added banner part with caresoul system and added text there

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import router from './Routes/Routes.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div className='max-w-7xl mx-auto'>
+  <div className='max-w-6xl mx-auto'>
     <React.StrictMode>
    <RouterProvider router={router} />
   </React.StrictMode>,
