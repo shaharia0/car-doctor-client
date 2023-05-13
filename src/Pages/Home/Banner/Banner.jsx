b292aef1813272f2e511890c086c2ba411fdbdf6
shaharia0 <shaharia35-1964@diu.edu.bd>
2023-05-13 12:05:00 +0600

    added about us section in home page under the hero section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
     <div className="carousel w-full h-[600px]">
     <div id="slide1" className="carousel-item relative w-full">
       <img src={img1} className="w-full rounded-xl" />
-      <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+      <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
         <div className='text-white space-y-7 w-1/2 pl-16'>
           <h2 className='text-6xl font-bold'>
             Affordable Price for Car Servicing
@@ -28,8 +28,8 @@ const Banner = () => {
       </div>
     </div> 
     <div id="slide2" className="carousel-item relative w-full">
-      <img src={img2} className="w-full" />
-      <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+      <img src={img2} className="w-full rounded-xl" />
+      <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
         <div className='text-white space-y-7 w-1/2 pl-16'>
           <h2 className='text-6xl font-bold'>
             Affordable Price for Car Servicing
@@ -46,9 +46,10 @@ const Banner = () => {
         <a href="#slide3" className="btn btn-circle">❯</a>
       </div>
     </div> 
+
     <div id="slide3" className="carousel-item relative w-full">
-      <img src={img3} className="w-full" />
-      <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+      <img src={img3} className="w-full rounded-xl" />
+      <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
         <div className='text-white space-y-7 w-1/2 pl-16'>
           <h2 className='text-6xl font-bold'>
             Affordable Price for Car Servicing
@@ -65,9 +66,10 @@ const Banner = () => {
         <a href="#slide4" className="btn btn-circle">❯</a>
       </div>
     </div> 
+
     <div id="slide4" className="carousel-item relative w-full">
-      <img src={img4} className="w-full" />
-      <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+      <img src={img4} className="w-full rounded-xl" />
+      <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
         <div className='text-white space-y-7 w-1/2 pl-16'>
           <h2 className='text-6xl font-bold'>
             Affordable Price for Car Servicing
@@ -84,9 +86,10 @@ const Banner = () => {
         <a href="#slide5" className="btn btn-circle">❯</a>
       </div>
     </div>
+
     <div id="slide5" className="carousel-item relative w-full">
-      <img src={img5} className="w-full" />
-      <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+      <img src={img5} className="w-full rounded-xl" />
+      <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]"> 
         <div className='text-white space-y-7 w-1/2 pl-16'>
           <h2 className='text-6xl font-bold'>
             Affordable Price for Car Servicing
@@ -103,9 +106,10 @@ const Banner = () => {
         <a href="#slide6" className="btn btn-circle">❯</a>
       </div>
     </div>
+
     <div id="slide6" className="carousel-item relative w-full">
-      <img src={img6} className="w-full" />
-      <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+      <img src={img6} className="w-full rounded-xl" />
+      <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
         <div className='text-white space-y-7 w-1/2 pl-16'>
           <h2 className='text-6xl font-bold'>
             Affordable Price for Car Servicing
