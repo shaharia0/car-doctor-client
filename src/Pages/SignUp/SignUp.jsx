03d2e4045513e231d5df3082675aa98d70cda429
shaharia0 <shaharia35-1964@diu.edu.bd>
2023-05-14 00:17:39 +0600

    added signup form in client side

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,20 +1,33 @@
 import { Link } from "react-router-dom";
 import img from "../../assets/images/login/login.svg";
-const Login = () => {
-  const handleLogin = (event) => {
+
+const SignUp = () => {
+  const handleSignUp = (event) => {
     event.preventDefault();
   };
 
   return (
-    <div className="hero min-h-screen bg-white">
+    <div>
+       <div className="hero min-h-screen bg-white">
       <div className="hero-content flex-col lg:flex-row">
         <div className="w-1/2 mr-12">
           <img src={img} alt="" />
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
-            <h1 className="text-4xl font-bold text-center">Login</h1>
-            <form onSubmit={handleLogin}>
+            <h1 className="text-3xl font-bold text-center">SignUp</h1>
+            <form onSubmit={handleSignUp}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="name"
+                  name="name"
+                  className="input input-bordered"
+                />
+              </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -28,7 +41,7 @@ const Login = () => {
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Password</span>
+                  <span className="label-text">Confirm Password</span>
                 </label>
                 <input
                   type="text"
@@ -46,21 +59,22 @@ const Login = () => {
                 <input
                   className="btn btn-error text-white bg-red-600"
                   type="submit"
-                  value="Login"
+                  value="SignUp"
                 />
               </div>
             </form>
             <p className="my-4 text-center">
-              New to Car Doctor?{" "}
-              <Link className="text-orange-600 font-bold" to="/signup">
-                Sign Up
+              Already have an Account?{" "}
+              <Link className="text-orange-600 font-bold" to="/login">
+                Login
               </Link>
             </p>
           </div>
         </div>
       </div>
     </div>
+    </div>
   );
 };
 
-export default Login;
+export default SignUp;
