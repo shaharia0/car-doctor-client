c3490dfabcdaa0093c36ca23b99951bb1950295e
shaharia0 <shaharia35-1964@diu.edu.bd>
2023-05-14 19:32:13 +0600

    added authprovider for firebase authentication

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,10 +1,24 @@
+import { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
 
 
-const AuthProvider = () => {
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const authInfo = {
+     user, 
+    loading
+  }
+
   return (
-    <div>
-      
-    </div>
+    <AuthContext.Provider value={authInfo}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
