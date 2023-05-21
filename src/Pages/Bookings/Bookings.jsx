9a93f5e02345388d67a25485b7728a5afddb8be6
shaharia0 <shaharia35-1964@diu.edu.bd>
2023-05-22 00:38:11 +0600

    update data section done

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -11,7 +11,48 @@ const Bookings = () => {
     fetch(url)
     .then(res => res.json())
     .then(data => setBookings(data))
-  },[url])
+  },[url]);
+
+  const handleDelete = id =>{
+    const proceed = confirm('Are you sure you want to delete ?');
+    if(proceed){
+            fetch(`http://localhost:5000/bookings/${id}`, {
+              method: 'DELETE',
+
+            })
+            .then(res=> res.json())
+            .then(data => {
+              console.log(data);
+              if(data.deletedCount > 0){
+                alert('deleted successfully !!!');
+                const remaining = bookings.filter(booking => booking._id !== id);
+                setBookings(remaining);
+              }
+            })
+    }
+  }
+
+  const handleBookingConfirm = id => {
+    fetch(`http://localhost:5000/bookings/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({status: 'confirm'})
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if(data.modifiedCount > 0){
+        // update state 
+         const remaining = bookings.filter(booking => booking._id !== id);
+         const updated = bookings.find(booking => booking._id === id);
+         updated.status = 'confirm'
+         const newBookings = [updated, ...remaining];
+         setBookings(newBookings);
+      }
+    })
+  }
   return (
     <div>
       <h2 className="text-5xl">Your Bookings: {bookings.length}</h2>
@@ -25,10 +66,11 @@ const Bookings = () => {
             <input type="checkbox" className="checkbox" />
           </label>
         </th>
-        <th>Photo</th>
+        <th>Image</th>
         <th>Service</th>
         <th>Date</th>
         <th>Price</th>
+        <th>Status</th>
         <th></th>
       </tr>
     </thead>
@@ -37,6 +79,8 @@ const Bookings = () => {
       bookings.map(booking => <BookingRow
       key={booking._id}
       booking={booking}
+      handleDelete={handleDelete}
+      handleBookingConfirm={handleBookingConfirm}
       ></BookingRow>)
      }
     </tbody>
